72c313ca0c657216be3730bd1b830f48b64a8e08
Radu Vrinceanu <radu.vrinceanu@ivfuture.uk>
2023-02-26 02:49:22 +0200

    added Services class inside the implementation

--- src/Bootstrapers/DatabaseBootstraper.java
+++ src/Bootstrapers/DatabaseBootstraper.java
@@ -1,4 +1,4 @@
-package Bootstrap;
+package Bootstrapers;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import Bootstrap.DatabaseBootstraper;
+import Bootstrapers.DatabaseBootstraper;
 
 public class Main {
     public static void main(String[] args) {
         var databaseInstance = DatabaseBootstraper.getInstance("jdbc:mysql://localhost:3306/auctions", "root", "");
+
+
 
         System.out.println("Hello world!");
     }
